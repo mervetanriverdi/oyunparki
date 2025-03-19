170e0961a803009b99872e278945aad747a7e7ae
mervetanriverdi <senin_github_emailin@example.com>
2025-03-19 14:02:37 +0300

    Harcamalar dosyası eklendi

---
+++ modules/muhasebe/harcamalar.py
@@ -0,0 +1,16 @@
+class Harcamalar:
+    def __init__(self, kategori, tutar):
+
+
+
+
+self.kategori = kategori
+        self.tutar = tutar
+
+
+
+ def __str__(self):
+
+
+
+       return f"Kategori: {self.kategori}, Tutar: {self.tutar} TL"
